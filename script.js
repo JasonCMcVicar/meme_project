129456c9496d5bc4508444f1f84537bfc2e193e7
Jason <jasoncmcvicar@gmail.com>
2022-05-15 01:46:41 -0500

    getting it figured out how to add the top and bottom text appended below the form

---
+++ script.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('form');
+const topText = document.querySelector('input[name="toptext"]');
+const bottomText = document.querySelector('input[name="bottomtext"]');
+const memePlace = document.querySelector('section');
+
+
+form.addEventListener('submit', function(event){
+  event.preventDefault();
+  //alert('you just made a meme!');
+  const wordFormA = makeTop(topText.value);
+  const wordFormB = makeBottom(bottomText.value);
+  memePlace.appendChild(wordFormA);
+  memePlace.appendChild(wordFormB);
+});
+
+function makeTop(top) {
+  topOf = document.createElement('h3');
+  topOf.innerText = top;
+  return topOf;
+}
+
+function makeBottom(bottom) {
+  bottomOf = document.createElement('h3');
+  bottomOf.innerText = bottom;
+  return bottomOf;
+}
