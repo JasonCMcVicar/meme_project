6b09037712fec2b6ee03d0b8be6a7cfa33fea5bc
Jason <jasoncmcvicar@gmail.com>
2022-05-16 13:28:17 -0500

    all working except for delete function

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ const bottomText = document.querySelector('input[name="bottomtext"]');
 const photo = document.querySelector('input[name="url"]');
 
 const memePlace = document.querySelector('section');
-
-
-
+let inputButton = document.createElement('button');
 
 form.addEventListener('submit', function(event){
   event.preventDefault();
@@ -16,21 +14,36 @@ form.addEventListener('submit', function(event){
   const wordFormA = makeTop(topText.value);
   const wordFormB = makeBottom(bottomText.value);
 
-  //create space for meme
+  //create div that will contain picture plus words
   const imageDiv = document.createElement('div');
+  imageDiv.className = 'oned';
 
-  //add image
-  memePlace.appendChild(imageDiv);
+  //create div that will contain picture plus words plus button
+  var containerDiv = document.createElement('div');
+  containerDiv.className = 'supreme';
+
+  //create button
+  inputButton = document.createElement('button');
+  inputButton.type = 'submit';
+  inputButton.innerText='DELETE';
+
   imageDiv.style.backgroundImage = `url(${photo.value})`;
-
   //add top and bottom text
   imageDiv.appendChild(wordFormA);
   imageDiv.appendChild(wordFormB);
+
+  containerDiv.appendChild(imageDiv);
+  containerDiv.appendChild(inputButton);
+
+  memePlace.appendChild(containerDiv);
+
+
 
   //reset inputs
   topText.value = '';
   bottomText.value = '';
   photo.value = '';
+
 });
 
 
@@ -45,3 +58,9 @@ function makeBottom(bottom) {
   bottomOf.innerText = bottom;
   return bottomOf;
 }
+
+
+inputButton.addEventListener('submit', function(event){
+  event.preventDefault();
+  console.log('hi');
+});
